9d3285ab073e59bb9cdd9529e91a0451ac1052ea
Mykola Dekhtiarenko <nnick1212@gmail.com>
2018-04-15 11:07:49 +0300

    improved mobile layout

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,35 +1,36 @@
-const urlsToCache = [
-    'index.html',
-    'css/appshell.css',
-    'js/main.js',
-    'js/jquery-3.3.1.js',
-    'fonts/icomoon.woff',
-    'css/style.css'
-]
+
+    const urlsToCache = [
+        'index.html',
+        'css/appshell.css',
+        'js/main.js',
+        'js/jquery-3.3.1.js',
+        'fonts/icomoon.woff',
+        'css/style.css'
+    ]
 
 
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open("static-content-v1")
-            .then(function (cache) {
-                cache.addAll(urlsToCache)
-            })
-    );
-});
+    self.addEventListener('install', function (event) {
+        event.waitUntil(
+            caches.open("static-content-v1")
+                .then(function (cache) {
+                    cache.addAll(urlsToCache)
+                })
+        );
+    });
 
-self.addEventListener('fetch', function (event) {
-    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-        return;
-    }
-    const url = new URL(event.request.url);
-    if (url.origin == location.origin && url.pathname == "/lightbulb") {
-        event.respondWith(caches.match("index.html"));
-        return;
-    }
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
-});
+    self.addEventListener('fetch', function (event) {
+        if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+            return;
+        }
+        const url = new URL(event.request.url);
+        if (url.origin == location.origin && url.pathname == "/lightbulb") {
+            event.respondWith(caches.match("index.html"));
+            return;
+        }
+        event.respondWith(
+            caches.match(event.request)
+                .then(response => response || fetch(event.request))
+        );
+    });
 
 
